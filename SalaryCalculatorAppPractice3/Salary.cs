58dcb00d3bf6c1017463362beddb224536533658
unknown <bosu15-5645@diu.edu.bd>
2019-10-15 00:15:06 +0600

    salary calculator

---
+++ SalaryCalculatorAppPractice3/Salary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    class Salary
+    {
+        public string employeeName;
+        public double basicAmount;
+        public double homeRent;
+        public double medicalAllowence;
+        
+        public double salaryReport(double basic, double hrPercentage, double maPercentage)
+        {
+            double MainSalary = 0.0;
+            MainSalary =basic+ ((basic * hrPercentage) / 100 + (basic * maPercentage) / 100);
+            return MainSalary;
+        }
+    }
+}
---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+        Salary salary = new Salary();
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+           string employeeName = employeeNameTextBox.Text;
+           double totalSalary= salary.salaryReport(Convert.ToDouble(basicAmountTextBox.Text), Convert.ToDouble(homeRentTextBox.Text), Convert.ToDouble(medicalAllowanceTextBox.Text));
+            MessageBox.Show(employeeName + " your salary is " + totalSalary);
+        }
+    }
+}
